8b2f0d18630749e064131963c21db263fa9fa007
Vaibhav Singh <60564482+winnerfool@users.noreply.github.com>
2022-02-18 12:34:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const x = document.querySelector("button");
 
-//x.innerHTML="hannibal";
 
-//button.addEventListener("click", alertButton, false);
-//const button = document.querySelector("button");
 
 /* Callback function */
 function alertButton() {
-   // alert('Hi native JavaScript');
-//  const response=fetch("https://api.quotable.io/random");
-//   const value=response.json;
-   
-  // document.querySelector("#quote").innerHTML = value.content;
+
 
  fetch("https://api.quotable.io/random").then(function (resp) {
     return resp.json(); 
@@ -22,22 +15,7 @@ function alertButton() {
    document.querySelector("#auth").innerHTML = data.author;
  });
 
-/*
-   const response = await fetch("https://api.quotable.io/random");
-   const data = await response.json();
-   if (response.ok) {
-     // Update DOM elements
-     quote.textContent = data.content;
-     cite.textContent = data.author;
-   } else {
-     quote.textContent = "An error occured";
-     console.log(data);
-   }
 
-
-    //document.getElementById("button").innerHTML = "boom";
-    document.querySelector("#quote").innerHTML = data;
-*/
 }
 
 
